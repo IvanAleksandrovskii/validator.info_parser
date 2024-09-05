e031c590fd73db914d0b35c3f3508ee94785a6a2
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-09-06 04:31:07 +0700

    fixes

--- core/parsing_validator_info/base_scraper.py
+++ core/parsing_validator_info/base_scraper.py
@@ -1,5 +1,5 @@
-import re
 from typing import List
+import re
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -10,29 +10,31 @@ from core import settings
 
 class ValidatorInfoScraper:
     def __init__(self, urls: List[str]):
-        self.urls = urls
-        self.driver = self._get_chrome_driver()
+        self.urls: List[str] = urls
+        self.driver: webdriver.Chrome = self._get_chrome_driver()
 
-    def _get_chrome_driver(self):
+    def _get_chrome_driver(self) -> webdriver.Chrome:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         service = Service(settings.chrome.path)
+
         return webdriver.Chrome(service=service, options=chrome_options)
 
-    def __del__(self):
+    def __del__(self) -> None:
         if hasattr(self, 'driver'):
             self.driver.quit()
 
     @staticmethod
-    def _clean_validator_name(name):
+    def _clean_validator_name(name:str) -> str:
         name = re.sub(r'^[+-]?\d+\s*', '', name)
         name = re.sub(r'\bNEW\s*', '', name)
         name = re.sub(r'^\d+\s*', '', name)
         name = re.sub(r'\s+', ' ', name).strip()
+
         return name
 
     @staticmethod
-    def _clean_numeric_value(value, column_name):
+    def _clean_numeric_value(value: str, column_name: str) -> str:
         value = value.split('\n')[0] if '\n' in value else value
 
         if column_name == "Votes":
